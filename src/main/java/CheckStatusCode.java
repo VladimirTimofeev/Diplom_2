1af0fab509f61a0882605244a430ed670f8269d2
Vladimir Timofeev <v.timofeev1977@ya.ru>
2025-03-17 16:29:07 +0300

    add variable HTTPComponents

--- src/main/java/CheckStatusCode.java
+++ src/main/java/CheckStatusCode.java
@@ -1,41 +1,40 @@
 import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 
 public class CheckStatusCode {
-
-//    private Response response;
 
     //Провека кода 200
     public void checkStatusCode200(Response response) {
         response.then().assertThat()
-                .statusCode(200);
+                .statusCode(HttpStatus.SC_OK);
     }
 
     public void checkStatusCode202(Response response) {
         response.then().assertThat()
-                .statusCode(202);
+                .statusCode(HttpStatus.SC_ACCEPTED);
     }
 
     //400
     public void checkStatusCode400(Response response) {
         response.then().assertThat()
-                .statusCode(400);
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
     }
 
     //401
     public void checkStatusCode401(Response response) {
         response.then().assertThat()
-                .statusCode(401);
+                .statusCode(HttpStatus.SC_UNAUTHORIZED);
     }
 
     //403
     public void checkStatusCode403(Response response) {
         response.then().assertThat()
-                .statusCode(403);
+                .statusCode(HttpStatus.SC_FORBIDDEN);
     }
 
     //500
     public void checkStatusCode500(Response response) {
         response.then().assertThat()
-                .statusCode(500);
+                .statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
     }
 }
